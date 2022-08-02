14667b61e52ed836d023770edbe715b617a12174
Shadman Shafi <shadmanshafi@iut-dhaka.edu>
2022-08-02 15:13:19 +0600

    need to reposition and restyle left-right arrow buttons

--- slider-component/src/App.js
+++ slider-component/src/App.js
@@ -1,7 +1,64 @@
-import "./App.css";
+import { useRef, useState } from "react";
 
-function App() {
-  return <div> Slider Component</div>;
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "./styles.css";
+
+// import required modules
+import { Navigation } from "swiper";
+
+import placeholderImage from "./placeholder.png"
+
+export default function App() {
+  return (
+    <div className="slider-entire-component-div">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+        {/*Slide 1*/}
+        <SwiperSlide>
+          <div className="slider-content-column">
+            <div className="slider-content-row">
+              <p className="slider-title">Key features and resources</p>
+            </div>
+            <div>
+              <p className="slider-list-item-title">Identity</p>
+              <ul>
+                <div className="slider-list-item-row">
+                  <p className="slider-list-item">•</p>
+                  <p className="slider-list-item">Users can be uniquely identified by several optional fields (e.g. id.
+usemane, email or mobile).</p>
+                </div>
+                <div className="slider-list-item-row">
+                  <p className="slider-list-item">•</p>
+                  <p className="slider-list-item">Each user object supports general-purpose fields for verifying identity information (e.g. first name, last name, profile photo, birth date, gender,
+etc.).</p>
+                </div>
+                <div className="slider-list-item-row">
+                  <p className="slider-list-item">•</p>
+                  <p className="slider-list-item">JSON metadata field is available for capturing any additional relevant
+information.</p>
+                </div>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <img src={placeholderImage} alt="placeholder-image"/>
+          </div>
+        </SwiperSlide>
+
+        {/*Slide 2*/}
+        <SwiperSlide>Slide 2</SwiperSlide>
+
+        {/*Slide 3*/}
+        <SwiperSlide>Slide 3</SwiperSlide>
+        
+        {/*Slide 4*/}
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
-
-export default App;
